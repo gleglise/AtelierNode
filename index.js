8030afaa02f5aacd6414c1ba0ac3ad60fd4caa23
Geoffrey <geoffreyleglise@gmail.com>
2024-10-15 11:54:27 +0200

    Atelier Node.js

---
+++ index.js
@@ -0,0 +1,8 @@
+//const wilders = ["Marwane", "Sarah", "Julien", "Geoffrey"];
+import chalk from 'chalk';
+
+const log = console.log;
+
+log(chalk.blue('Marwane ') + chalk.red('Sarah ') + chalk.green('Julien ') + chalk.yellow('Geoffrey'));
+
+console.log(log)
